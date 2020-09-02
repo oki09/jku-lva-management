2cef3d47008853bdbc7c960a753f490dd6f64f3e
Oktay Akgül <oktay.akguel@enjoyit.at>
2020-09-02 22:23:45 +0200

    update php mail()

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,8 +17,13 @@ class ContactController extends Controller
         $to = env('MAIL_TO_ADDRESS');
         $subject = 'Feedback from ' . $feedback['email'];
         $msg = wordwrap($feedback['message'], 70);
-        $headers = 'From: ' . $feedback['name'] . ' <' . $feedback['email'] . '>';
-        if(mail($to, $subject, $msg, $headers))
+        $headers = [
+            'From' => $feedback['name'] . ' <' . $feedback['email'] . '>',
+            'X-Mailer' => 'PHP/' . phpversion(),
+            'MIME-Version' => '1.0',
+            'Content-type' => 'text/plain; charset=utf-8'
+        ];
+        if (mail($to, $subject, $msg, $headers, '-f' . $to))
             return redirect()->route('info.contact')->with('isSent', true);
         else
             return redirect()->route('info.contact')->with('isSent', false);
